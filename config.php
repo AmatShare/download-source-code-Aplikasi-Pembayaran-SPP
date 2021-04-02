3ed8e56c930f808e071af98df197048ba45a3216
AmatShare <81802193+AmatShare@users.noreply.github.com>
2021-04-02 15:48:12 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$host       = "sql310.epizy.com";
+$user       = "epiz_28031632";
+$pass       = "AiEi7DF8g5";
+$database   = "epiz_28031632_db_ujikom";
+
+$koneksi = new mysqli($host,$user,$pass,$database);
+
+?>
